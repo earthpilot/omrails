433f1135f295f9ee0b15398f06ad5c54493cde1c
Todd Higgins <earthpilot@gmail.com>
2013-04-17 22:31:17 -0400

    Add User show action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
     @pins = @user.pins.page(params[:page]).per_page(20)
   end
 end
